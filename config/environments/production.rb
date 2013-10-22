298113c2078e33c7966b44849d698c2569ab809e
Rosengren <krosengr@gmail.com>
2013-10-22 19:30:10 -0400

    added jQuery Masonry

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,14 @@ Pinteresting::Application.configure do
   # Note: set this to actual host
   config.action_mailer.default_url_options = { :host => 'http://salty-shore-6120.herokuapp.com' }
 
+  # Sets paperclip to upload images to Amazon S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+  
 end
